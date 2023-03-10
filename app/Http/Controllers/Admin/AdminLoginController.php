53c61f29da9f6ba8cc4844f1400f3721ab260d2c
Bilal ahmad <bilal.ahmad@obcc.de>
2023-03-10 16:41:36 +0100

    admin model done

--- app/Http/Controllers/Admin/AdminLoginController.php
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Admin;
+use Auth;
+use Hash;
 
 class AdminLoginController extends Controller
 {
