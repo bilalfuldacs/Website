d855576a1b4ed005a3af815799e0928460d85b11
Bilal ahmad <bilal.ahmad@obcc.de>
2023-03-16 10:23:03 +0100

    home page and other layouts added

---
+++ resources/views/front/layout/style.blade.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="{{asset('dist_front/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('dist_front/css/jquery-ui.css')}}">
+<link rel="stylesheet" href="{{asset('dist_front/css/animate.min.css')}}">
+<link rel="stylesheet" href="{{asset('dist_front/css/magnific-popup.css')}}">
+<link rel="stylesheet" href="{{asset('dist_front/css/owl.carousel.min.css')}}">
+<link rel="stylesheet" href="{{asset('dist_front/css/select2.min.css')}}">
+<link rel="stylesheet" href="{{asset('dist_front/css/select2-bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('dist_front/css/sweetalert2.min.css')}}">
+<link rel="stylesheet" href="{{asset('dist_front/css/spacing.css')}}">
+<link rel="stylesheet" href="{{asset('dist_front/css/font_awesome_5_free.min.css')}}">
+<link rel="stylesheet" href="{{asset('dist_front/css/style.css')}}">
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\AdminLoginController;
 use App\Http\Controllers\Admin\AdminProfileController;
+use App\Http\Controllers\front\AdminHomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +16,8 @@ use App\Http\Controllers\Admin\AdminProfileController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+route::view('about', 'front.about');
+route::get('/', [AdminHomeController::class,'index'])->name('home') ;
 route::get('admin/home', [AdminController::class,'index'])->name('admin.home')->middleware('Admin:admin');
 route::get('admin/login', [AdminLoginController::class,'index'])->name('admin.Login');
 route::post('admin/login_submit', [AdminLoginController::class,'login_submit'])->name('admin.login_submit');
